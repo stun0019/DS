e341e42bd224860a51cf2bed9397abce3ad9efbf
overdose <ootdacc519@gmail.com>
2026-08-14 09:39:24 +0800

    Update volumePanel.js

--- assets/js/panels/volumePanel.js
+++ assets/js/panels/volumePanel.js
@@ -1,1 +1,77 @@
+import {
+  getTradeVolumeShares
+} from "../data/stockData.js";
 
+import {
+  renderStockPanel
+} from "../ui/stockTable.js";
+
+
+export function renderVolumePanel(
+  root,
+  stocks,
+  {
+    sortState,
+    onSort
+  }
+) {
+
+  const candidates =
+    [...stocks]
+
+    .filter(
+      stock =>
+        getTradeVolumeShares(
+          stock
+        ) > 0
+    )
+
+    .sort(
+      (
+        a,
+        b
+      ) =>
+
+        getTradeVolumeShares(
+          b
+        )
+
+        -
+
+        getTradeVolumeShares(
+          a
+        )
+    )
+
+    .slice(
+      0,
+      10
+    );
+
+
+  return renderStockPanel(
+    root,
+    {
+      data:
+        candidates,
+
+      rankMode:
+        true,
+
+      statusText:
+        `成交量 TOP ${candidates.length}`
+        +
+        "｜上市＋上櫃"
+        +
+        "｜券商可比成交量",
+
+      sortState,
+
+      onSort,
+
+      panelClass:
+        "panel-volume"
+    }
+  );
+
+}
