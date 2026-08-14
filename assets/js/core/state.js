18a359e761718523f6476fdfc093753e7d341146
overdose <ootdacc519@gmail.com>
2026-08-14 09:34:04 +0800

    Update state.js

--- assets/js/core/state.js
+++ assets/js/core/state.js
@@ -1,1 +1,75 @@
+export const state = {
 
+  stocks: [],
+
+  metadata: {},
+
+  pageReadAt: "",
+
+  currentView: "long",
+
+  currentItems: [],
+
+  sortState: {
+    key: null,
+    direction: null,
+    type: null
+  }
+
+};
+
+
+export function setStocks(
+  stocks,
+  metadata,
+  pageReadAt
+) {
+
+  state.stocks =
+    stocks;
+
+
+  state.metadata =
+    metadata;
+
+
+  state.pageReadAt =
+    pageReadAt;
+
+}
+
+
+export function setCurrentItems(
+  items
+) {
+
+  state.currentItems =
+    items;
+
+}
+
+
+export function setSort(
+  key,
+  direction,
+  type
+) {
+
+  state.sortState = {
+    key,
+    direction,
+    type
+  };
+
+}
+
+
+export function resetSort() {
+
+  state.sortState = {
+    key: null,
+    direction: null,
+    type: null
+  };
+
+}
