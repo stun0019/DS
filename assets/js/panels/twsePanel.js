345958495223cf7aaac2dacdfb047b45a1baef57
overdose <ootdacc519@gmail.com>
2026-08-14 09:40:07 +0800

    Update twsePanel.js

--- assets/js/panels/twsePanel.js
+++ assets/js/panels/twsePanel.js
@@ -1,1 +1,42 @@
+import {
+  renderStockPanel
+} from "../ui/stockTable.js";
 
+
+export function renderTwsePanel(
+  root,
+  stocks,
+  {
+    sortState,
+    onSort
+  }
+) {
+
+  const data =
+    stocks.filter(
+      stock =>
+        stock.Market ===
+        "TWSE"
+    );
+
+
+  return renderStockPanel(
+    root,
+    {
+      data,
+
+      statusText:
+        `上市股票 ${data.length.toLocaleString(
+          "zh-TW"
+        )} 檔`,
+
+      sortState,
+
+      onSort,
+
+      panelClass:
+        "panel-market"
+    }
+  );
+
+}
