05b0139aca2fa22b9ab6aaaf30933b9aa17742a5
overdose <ootdacc519@gmail.com>
2026-08-14 09:34:18 +0800

    Update router.js

--- assets/js/core/router.js
+++ assets/js/core/router.js
@@ -1,1 +1,62 @@
+export function getInitialView(
+  validViews
+) {
 
+  const hash =
+    location.hash
+    .replace(
+      "#",
+      ""
+    )
+    .trim()
+    .toLowerCase();
+
+
+  if (
+    validViews.includes(
+      hash
+    )
+  ) {
+
+    return hash;
+
+  }
+
+
+  return "long";
+
+}
+
+
+export function writeViewToHash(
+  view
+) {
+
+  history.replaceState(
+    null,
+    "",
+    `#${view}`
+  );
+
+}
+
+
+export function bindHashNavigation(
+  validViews,
+  callback
+) {
+
+  window.addEventListener(
+    "hashchange",
+    () => {
+
+      callback(
+        getInitialView(
+          validViews
+        )
+      );
+
+    }
+  );
+
+}
