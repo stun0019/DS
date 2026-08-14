060a080d16b46f56a6d23ea2827ee5487fa9cd01
overdose <ootdacc519@gmail.com>
2026-08-14 09:40:18 +0800

    Update tpexPanel.js

--- assets/js/panels/tpexPanel.js
+++ assets/js/panels/tpexPanel.js
@@ -1,1 +1,42 @@
+import {
+  renderStockPanel
+} from "../ui/stockTable.js";
 
+
+export function renderTpexPanel(
+  root,
+  stocks,
+  {
+    sortState,
+    onSort
+  }
+) {
+
+  const data =
+    stocks.filter(
+      stock =>
+        stock.Market ===
+        "TPEX"
+    );
+
+
+  return renderStockPanel(
+    root,
+    {
+      data,
+
+      statusText:
+        `上櫃股票 ${data.length.toLocaleString(
+          "zh-TW"
+        )} 檔`,
+
+      sortState,
+
+      onSort,
+
+      panelClass:
+        "panel-market"
+    }
+  );
+
+}
